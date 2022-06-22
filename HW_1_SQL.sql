d3d3e5541ddf4b4e88d82a0ee50f6b102693573c
kirillpomazov <kirillpomazov@gmail.com>
2022-06-22 19:08:55 +0200

    Update HW_1_SQL.sql
    Edit

--- HW_1_SQL.sql
+++ HW_1_SQL.sql
@@ -16,7 +16,7 @@ select * from students where password = '12333';
 
 select * from students where created_on = '2021-03-26 00:00:00';
 
-select * from students where name like 'Anna%';
+select * from students where name like '%Anna%';
 
 select * from students where name like '%8';
 
@@ -26,7 +26,7 @@ select * from students where created_on = '2021-07-12 00:00:00';
 
 select * from students where created_on = '2021-07-12 00:00:00'and password = '1m313';
 
-select * from students where created_on = '2021-07-12 00:00:00'and name like 'Andrey%';
+select * from students where created_on = '2021-07-12 00:00:00'and name like '%Andrey%';
 
 select * from students where created_on = '2021-07-12 00:00:00'and name like '%8%';
 
@@ -40,7 +40,7 @@ select * from students where id < '130';
 
 select * from students where id < '127' or id > '188' ; 
 
-select * from students where id <= '127';
+select * from students where id <= '137';
 
 select * from students where id >= '137';
 
